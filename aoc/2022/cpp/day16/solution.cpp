49e673bfe5788a907d7e30f246946d5d4294830c
shikharbhardwaj <shikhar.bhardwaj@instabase.com>
2022-12-19 22:01:52 +0530

    Add day16

--- aoc/2022/cpp/day16/solution.cpp
+++ aoc/2022/cpp/day16/solution.cpp
@@ -47,7 +47,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 #ifndef ONLINE_JUDGE
-    ifstream fin("in.txt");
+    ifstream fin("in2.txt");
     ofstream fout("out.txt");
     auto cinbuf = cin.rdbuf(fin.rdbuf());    // save and redirect
     auto coutbuf = cout.rdbuf(fout.rdbuf()); // save and redirect
@@ -70,19 +70,76 @@ int main() {
         }
     }
 
-    int max = 0;
-    set<string> visited;
+    vector<string> nodes;
+    for (auto [node, _] : graph) {
+        nodes.push_back(node);
+    }
+    sort(ALL(nodes));
+    vector<int> flow_rate_by_idx(nodes.size());
+    for (int i = 0; i < nodes.size(); ++i) {
+        flow_rate_by_idx[i] = flow_rate[nodes[i]];
+    }
 
-    auto max = [&](const string& node, int cur, int t, string parent, map<string, bool> valve_state) {
-        // We always turn on the current valve if it has > 0 value.
-        if (flow_rate[node] > 0) {
-            t -= 1;
-            // cur += flow_rate * ;
+    cout << nodes << endl;
+    cout << flow_rate_by_idx << endl;
+
+    using State = tuple<int, int, ull, int, ull>;
+    map<State, int> dp;
+
+    ull progress_counter = 0;
+    ull progress_batch = 1e6;
+
+    /*
+    Cases:
+    1. Elephant and me on the same node: elephant moves onto the neighbour, and I try to see if
+    turning on the current valve is useful.
+    2. Elephant and me on different nodes: both explore with/without turning on the current valve.
+    */
+
+    function<ull(int, int, ull, int, ull)> visit = [&](int node0_idx, int node1_idx, ull cur, int t, ull bits) -> ull {
+        if (t <= 0) return cur;
+        
+        auto cur_state = make_tuple(node0_idx, node1_idx, cur, t, bits);
+        // cout << cur_state << endl;
+
+        if (dp.count(cur_state) != 0) {
+            // cout << "Found existing state" << endl;
+            return dp[cur_state];
         }
+        ++progress_counter;
+
+        if (progress_counter % progress_batch == 0) {
+            cout << "Visited: " << progress_counter << endl;
+        }
+
+        // cout << "Visit: " << cur_state << endl;
+        ull best = cur;
+
+       string node0 = nodes[node0_idx];
+       string node1 = nodes[node1_idx];
+
+        for (auto nei0 : graph[node0]) {
+            int nei0_idx = lower_bound(ALL(nodes), nei0) - begin(nodes);
+            best = max(best, visit(nei0_idx, node1_idx, cur, t - 1, bits));
+        }
+
+        if (!(bits & (1 << node0_idx)) && flow_rate_by_idx[node0_idx] > 0) {
+            cur += (t - 1) * flow_rate[node0];
+
+            bits |= (1 << node0_idx);
+
+            for (auto nei0 : graph[node0]) {
+                int nei0_idx = lower_bound(ALL(nodes), nei0) - begin(nodes);
+                best = max(best, visit(nei0_idx, node1_idx, cur, t - 2, bits));
+            }
+        }
+
+        return dp[cur_state] = best;
     };
 
     cout << graph << endl;
     cout << flow_rate << endl;
+    cout << visit(0, 0, 0, 30, 0) << endl;
 #ifndef ONLINE_JUDGE
     cin.rdbuf(cinbuf);    // restore
     cout.rdbuf(coutbuf); // restore
